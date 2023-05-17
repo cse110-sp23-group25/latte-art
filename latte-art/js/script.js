313ee48c9d0cd95ca44d9effd8165e0e2b5baa6f
alexanderyoza <alex.yoza@gmail.com>
2023-05-17 15:36:33 -0700

    creates seed generation and fortune generation functions : fixes #6 and fixes #7

--- latte-art/js/script.js
+++ latte-art/js/script.js
@@ -1,1 +1,31 @@
 
+
+const POSSIBLE_FORTUNES = ['Fortune 1', 'Fortune 2', 'Fortune 3', 'Fortune 4', 'Fortune 5', 'Fortune 6', 'Fortune 7', 'Fortune 8', 'Fortune 9', 'Fortune 10']; // TODO: add ten different fortunes
+
+/**
+ * generates a four-digit seed based on the the state of the latte art
+ * Params:
+ *  stats : int - the amount of 'milk' added to the coffee
+ * Return:
+ *  int - four-digit */
+function seedGeneration(stats) { // TODO: once we know what the amount value looks like we will change this function
+    const seed = Math.floor(Math.random() * stats);
+    if(seed > 1000)
+        return 1000;
+    if (seed < 0)
+        return 0;
+    
+    return seed;
+}
+
+/**
+ * generates generates a fortune based on the given amount of milk using the seedGeneration() function
+ * Params:
+ *  stats : int - the amount of 'milk' added to the coffee
+ * Return:
+ *  fortune: string */
+function fortuneSelector(stats) {
+    const seed = seedGeneration(stats);
+    const fortune = seed % 10; 
+    return POSSIBLE_FORTUNES[fortune];
+}
