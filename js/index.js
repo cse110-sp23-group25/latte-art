1ee11bbb63b2f5ed420dba9e391577457efd7457
alexanderyoza <alex.yoza@gmail.com>
2023-06-11 05:23:36 -0700

    linter changes

--- js/index.js
+++ js/index.js
@@ -4,31 +4,32 @@ window.onload = function () {
 }
 
 function init () {
-    let volbtn = document.getElementById('controls');
-    volbtn.addEventListener('click', volumeControl);
+    let volbtn = document.getElementById('controls')
+    volbtn.addEventListener('click', volumeControl)
 
-    getNews()
+    getFacts()
 }
 
-// fetch news from api
-async function getNews() {
+// fetch facts from json
+async function getFacts() {
     try {
-        const apiurl = await (await fetch('../assets/news.json')).json()
-        const news = await (await fetch(apiurl.path)).json()
-        const items = document.getElementsByClassName('ticker-item')
+        const info = await (await fetch('../assets/facts.json')).json()
+        const idx = Math.floor(Math.random() * 5)
+        const transition = document.createElement('div')
+        transition.className = 'ticker-transition'
 
-        for (let i = 0; i < news.articles.length; i++) {
-            items[i].innerHTML = 
-                `
-                <a href="${news.articles[i].url}" target="_blank" rel="noopener noreferrer">
-                    <img src="${news.articles[i].urlToImage}" alt="news story">
-                    <div>${news.articles[i].title}</div>
-                </a>
-                `
+        for (let i = 0; i < 5; i++) {
+            const itm = document.createElement('div')
+            itm.className = 'ticker-item'
+            itm.innerHTML = info.facts[5 * i + idx]
+            transition.appendChild(itm)
         }
 
+        const wrapper = document.getElementById('ticker-wrapper')
+        wrapper.appendChild(transition)
+
     } catch (error) {
-        console.error('Error fetching news')
+        console.error('Error fetching facts')
         console.error(error)
     }
 
@@ -36,8 +37,8 @@ async function getNews() {
 
 
 function volumeControl() {
-    let icon = document.querySelector('#controls img');
-    let aud = document.querySelector('audio');
+    let icon = document.querySelector('#controls img')
+    let aud = document.querySelector('audio')
 
     if(aud.volume != 0) {
         aud.volume= 0;
@@ -45,6 +46,6 @@ function volumeControl() {
     } 
     else {
         aud.volume = 1;
-        icon.src = './assets/volumeon.png';
+        icon.src = './assets/volumeon.png'
     }
 }
